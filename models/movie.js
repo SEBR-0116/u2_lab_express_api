390a7aab1af17f45f206d3c64671cb46e16b51d8
AbeGue02 <abrahamgue02@gmail.com>
2024-02-09 17:00:15 -0500

    Adds models

---
+++ models/movie.js
@@ -0,0 +1,16 @@
+const  { Schema } = require('mongoose')
+
+const Movie = new Schema(
+    {
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        runtime_in_minutes: { type: Number, required: true },
+        date_released: { type: Date, required: true },
+        cast: { type: [{type: Schema.Types.ObjectId, ref: "Actor"}], required: true },
+        reviews: { type: [Review], required: true },
+        image: { type: String, required: true },
+    },
+    { timestamps: true },
+)
+
+module.exports = Movie
